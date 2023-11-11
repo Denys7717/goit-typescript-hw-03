7117b7b9bc73f5d01a73934489047a58bef4007a
Denys7717 <denys.rdbua@gmail.com>
2023-11-11 18:57:34 +0200

    upd

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,40 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+abstract class House {
+  public door: boolean;
+  public key: Key;
+  public tenants: Person[] = [];
 
-house.openDoor(person.getKey());
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
 
-house.comeIn(person);
-
-
-export {};
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Open");
+    }
+  }
+}
